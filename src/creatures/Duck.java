65ffe1f7de4b9462db149a4cca5a14eaa4f7046e
Vincent BOUCHER-THOUVENY <vincent.boucher06@gmail.com>
2015-12-10 11:21:18 +0100

    Working Simulator and animals + human classes

---
+++ src/creatures/Duck.java
@@ -0,0 +1,18 @@
+package creatures;
+
+import graph.Field;
+import graph.Location;
+
+/**
+ * Duck
+ *
+ * @author Plague Inc.
+ * @version 2015
+ */
+public class Duck extends Animal {
+
+    public Duck(Field field, Location location) {
+        super(field, location);
+    }
+
+}
